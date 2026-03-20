3f6388ba42ee39d7b99dafc9258c34c223532f5e
varundeepakgudhe <vgudhe@ncsu.edu>
2026-03-20 13:21:45 -0500

    added dotenv to read env file

--- app/db.py
+++ app/db.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 import os
 
-# Change later to RDS URL
+
+load_dotenv()
+
+
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://postgres:postgres@localhost:5432/urlshortener"
 )
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
